5df4999f570a03d04fe1d3de6f30ca370413695d
Alessandro Scala <alessandro.scala8@gmail.com>
2018-10-17 20:24:44 +0200

    Working PageViewController

--- MemoRise/GameViewController.swift
+++ MemoRise/GameViewController.swift
@@ -8,16 +8,18 @@
 
 import UIKit
 
-class GameViewController: UIPageViewController {
+class GameViewController: UIPageViewController, UIPageViewControllerDataSource{
     var currentIndex: Int = 0
     var currentTopic : Topic = Topic("")
     var questions : [Question] = []
     var mode: [Bool] = [false, false, true]
     var questionViewControllers : [QuestionViewController] = []
     //0 tempo 1 random 2 navigazione
+//    let storyboard = UIStoryboard(name: "Main", bundle: nil);
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        dataSource = self;
         loadTopic()
         if mode[1] {
             questions = currentTopic.getShuffledQuestions()
@@ -25,12 +27,13 @@ class GameViewController: UIPageViewController {
             questions = currentTopic.questions
         }
         
-        //        for i in 0..<questions.count{
-        //            questionViewControllers.append(QuestionViewController())
-        //            print(questions[i].question)
-        //            questionViewControllers[i].loadQuestion(inQuestion: questions[i])
-        //        }
-        self.setViewControllers(questionViewControllers, direction: .forward, animated: true, completion: nil)
+        for i in 0..<questions.count{
+            questionViewControllers.append(storyboard?.instantiateViewController(withIdentifier: "QuestionViewController") as! QuestionViewController);
+            print(questions[i].question)
+            questionViewControllers[i].Qindex = i;
+            questionViewControllers[i].questionObj = questions[i];
+        }
+        self.setViewControllers([questionViewControllers[0]], direction: .forward, animated: true, completion: nil)
     }
     
     
@@ -42,6 +45,25 @@ class GameViewController: UIPageViewController {
         currentTopic.addQuestion(question: "What is the square root of 4?", answers: ["2","4","8","16"], correctAnswer: 0);
         currentTopic.addQuestion(question: "2-12", answers: ["-10", "8", "10", "2"], correctAnswer: 0);
     }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        let qIndex = (viewController as! QuestionViewController).Qindex;
+        if qIndex < questionViewControllers.count-1 {
+            return questionViewControllers[qIndex+1];
+        } else {
+            return nil;
+        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        let qIndex = (viewController as! QuestionViewController).Qindex;
+        if qIndex > 0 {
+            return questionViewControllers[qIndex-1];
+        } else {
+            return nil;
+        }
+    }
+    
     //    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
     //        guard let pagesCount = data?.filteredProfiles.count else {
     //            return nil
@@ -74,15 +96,5 @@ class GameViewController: UIPageViewController {
     //        return profile
     //    }
     //}
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
--- MemoRise/QuestionViewController.swift
+++ MemoRise/QuestionViewController.swift
@@ -10,22 +10,27 @@ import UIKit
 
 class QuestionViewController: UIViewController {
     var Qindex : Int = -1;
+    var questionObj: Question?;
     
-    @IBOutlet weak var question: UILabel!
+    @IBOutlet var question: UILabel!
     @IBOutlet var answers: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        // Do any additional setup after loading the view.
+        loadQuestion(inQuestion: questionObj!);
     }
     
-    func loadQuestion(inQuestion: Question, index: Int) {
-        //        self.question.text = inQuestion.question
-        //        for i in 0..<inQuestion.answers.count{
-        //            self.answers[i].isHidden = false
-        //        self.answers[i].setTitle(inQuestion.answers[i], for: .normal)
-        //        self.index = index
+    func loadQuestion(inQuestion: Question) {
+        //Istantiating layout
+        /*question = UILabel();
+        answers = [];
+        for i in 0..<4{
+            answers.append(UIButton());
+        }*/
+        self.question.text = inQuestion.question
+        for i in 0..<inQuestion.answers.count{
+            self.answers[i].isHidden = false
+            self.answers[i].setTitle(inQuestion.answers[i], for: .normal)
+        }
     }
 }
